734b326a0478462733e538e5aa13a9bb05a969e5
sirichartchai <gs3802@nyu.edu>
2025-12-09 15:55:01 -0500

    feat: DEPLOY PYTHON ANYWHERE update deploy file for auto deploy on python anywhere

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # This shell script deploys a new version to a server.
 
-PROJ_DIR=demo-repo4
-VENV=Fall2023
-PA_DOMAIN="Fall2023.pythonanywhere.com"
-PA_USER='Fall2023'
+PROJ_DIR="team-bemg"
+PA_USER="teamBemg"
+PA_DOMAIN="teambemg.pythonanywhere.com"
+# Absolute path to the virtualenv used by the web app
+VENV_PATH="/home/${PA_USER}/team-bemg/.venv"
 echo "Project dir = $PROJ_DIR"
 echo "PA domain = $PA_DOMAIN"
-echo "Virtual env = $VENV"
+echo "Virtual env = $VENV_PATH"
 
 if [ -z "$DEMO_PA_PWD" ]
 then
@@ -19,6 +20,12 @@ echo "PA user = $PA_USER"
 echo "PA password = $DEMO_PA_PWD"
 
 echo "SSHing to PythonAnywhere."
-sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
-    cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN ./rebuild.sh
+sshpass -p "$DEMO_PA_PWD" ssh -o "StrictHostKeyChecking no" "$PA_USER@ssh.pythonanywhere.com" << EOF
+    set -e
+    cd "/home/${PA_USER}/${PROJ_DIR}"
+    PA_USER="${PA_USER}" \
+    PROJ_DIR="/home/${PA_USER}/${PROJ_DIR}" \
+    VENV_PATH="${VENV_PATH}" \
+    PA_DOMAIN="${PA_DOMAIN}" \
+    ./rebuild.sh
 EOF
